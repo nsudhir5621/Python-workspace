172307c4a14d7b7166956b785d249e29c35d13dd
Sudhir Nishad <nsudhir1265@gmail.com>
2025-09-25 17:46:03 +0530

    Add files via upload

---
+++ week1/day_7.py
@@ -0,0 +1,29 @@
+# Hacker Rank Problems
+
+
+# HackerRank: Calendar Module
+# Problem: Determine day of a given date
+# Link: https://www.hackerrank.com/challenges/calendar-module/problem
+# import calendar
+# month, day, year = map(int, input().split())
+# weekday = calendar.weekday(year, month, day)
+# days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+# print(days[weekday])
+# Notes: Used calendar (datetime-related), mapped weekday to day name
+
+# from datetime import datetime
+#
+# date_input = input()
+# parsed = datetime.strptime(date_input, "%d-%m-%Y")
+# print(f"Day of the week: {parsed.strftime('%A')}")
+# print(f"Month: {parsed.strftime('%B')}")
+# print(f"Year: {parsed.year}")
+
+
+
+
+
+
+
+
+
