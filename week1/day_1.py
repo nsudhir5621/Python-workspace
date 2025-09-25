24c15719965292fec9641c4a5e68a34054942958
Sudhir Nishad <nsudhir1265@gmail.com>
2025-09-25 17:43:36 +0530

    Add files via upload

---
+++ week1/day_1.py
@@ -0,0 +1,51 @@
+# Hacker Rank Problems
+
+# Python: "Say, Hello World"
+
+# print("Hello World")
+
+
+# Python: If else
+# Task
+# Given an integer,n, perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+# n = int(input())
+# if n % 2 != 0: #To check that the number is odd
+#     print("Weird")
+# else:
+#     if 2 <= n <= 5:
+#         print("Not Weird")
+#     elif 6 <= n <= 20:
+#         print("Weird")
+#     elif n > 20:
+#         print("Not Weird")
+
+
+# Python: Division and Floor division
+
+# a = int(input())
+# b = int(input())
+# print(a // b)
+# print(a / b)
+
+
+# Python: Arithmetic Operators
+# Add code to print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+# a = int(input())
+# b = int(input())
+# print (a+b)
+# print (a-b)
+# print (a*b)
+
+
+
+
+
